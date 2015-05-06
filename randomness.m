b3a7c98ebc0cce591f7182f2e48662a93e016bfa
Amr <amr.e.dev@gmail.com>
2015-05-06 16:05:41 +0300

    initial part of problem 4 -- still wierd error from the grader

---
+++ randomness.m
@@ -0,0 +1,15 @@
+% Introduction to Programming with MATLAB
+% Assignment 4 - Problem 3
+
+% input arguments: limit, n, and m,
+% returns an n-by-m matrix
+% of uniformly distributed random integers
+% between 1 and limit inclusive
+% You are not allowed to use randi
+% you can use rand
+% test the output with hist to make sure it's uniformally distributed
+
+function UniMat = randomness(limit, n, m)
+    UniMat = 1+ floor(rand(n,m) * (limit));
+end
+
